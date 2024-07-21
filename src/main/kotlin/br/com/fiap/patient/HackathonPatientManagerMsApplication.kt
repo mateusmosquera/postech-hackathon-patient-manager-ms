f90e915a5e443229e6b09403a0bca7ee97a6b867
mateusmosquera <mateusmosquera@hotmail.com>
2024-07-21 20:23:47 -0300

    init projeto

---
+++ src/main/kotlin/br/com/fiap/patient/HackathonPatientManagerMsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.patient.hackathon_patient_manager_ms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HackathonPatientManagerMsApplication
+
+fun main(args: Array<String>) {
+	runApplication<HackathonPatientManagerMsApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/fiap/patient/hackathon_patient_manager_ms/HackathonPatientManagerMsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.patient.hackathon_patient_manager_ms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HackathonPatientManagerMsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
